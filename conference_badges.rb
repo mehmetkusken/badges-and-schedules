599f72a11abb14599d8fe2ae8b6e6a7b13f69b9c
mehmetkusken <74083093+mehmetkusken@users.noreply.github.com>
2021-02-05 21:52:46 -0700

    Update conference_badges.rb

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,36 @@
-# Write your code here.
+
+def badge_maker(name = "Arel")
+    "Hello, my name is #{name}."
+end
+  
+def batch_badge_creator(attendees)
+    badges = []
+    attendees.each do |attendee|
+    badges<< "Hello, my name is #{attendee}."
+    end
+    badges
+end
+  
+def assign_rooms(attendees)
+    rooms = []
+    attendees.each_with_index do |attendee, index|
+      rooms<<"Hello, #{attendee}! You'll be assigned to room #{index+1}!"
+    end
+     rooms
+end
+
+def printer(attendees)
+    assign_rooms(attendees)
+    attendees.each do |attendee|
+        puts "Hello, my name is #{attendee}."
+    end
+    batch_badge_creator(attendees)
+    index = 1
+    attendees.each do |attendee| 
+    puts "Hello, #{attendee}! You'll be assigned to room #{index}!"
+        index += 1
+    end
+end
+  
+
+
